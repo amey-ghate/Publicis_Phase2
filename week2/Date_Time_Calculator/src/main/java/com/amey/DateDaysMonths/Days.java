e3cc269ce29f8b863f1d936f37e31283b8af423b
amey-ghate <amey.ghate@gmail.com>
2020-08-27 14:07:43 +0530

    Date_time_Calculator

---
+++ week2/Date_Time_Calculator/src/main/java/com/amey/DateDaysMonths/Days.java
@@ -0,0 +1,13 @@
+package com.amey.DateDaysMonths;
+
+public enum Days {
+
+	MONDAY,
+	Tuesday,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+	
+}
---
+++ week2/Date_Time_Calculator/src/test/com/amey/DateTimeCalculator/App_test.java
@@ -0,0 +1,20 @@
+package com.amey.DateTimeCalculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit testing for simple Application.
+ */
+public class AppTest 
+{
+    /**
+     * Testing 
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
